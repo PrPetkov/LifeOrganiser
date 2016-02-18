2265f29a3e67ebe83ead73de740a62c58a1496e6
Nikolay Hristov <niki_breik@abv.bg>
2016-02-18 02:23:45 +0200

    no message

---
+++ src/Models/accounts/Account.java
@@ -0,0 +1,67 @@
+package Models.accounts;
+
+
+import Exceptions.AccountException;
+import Interfaces.IAccount;
+import Models.User;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public abstract class Account implements IAccount {
+	
+	private String accountName;
+	private Currency currency;
+	private BigDecimal amount;
+
+	public Account(String accountName, Currency currency, BigDecimal amount) {
+
+		this.setAccountName(accountName);
+		this.setCurrency(currency);
+        try{
+		this.setAmount(amount);
+        } catch (AccountException e){
+            //TODO throw exception to the user
+        }
+
+	}
+
+	public Currency getCurrency() {
+		return currency;
+	}
+
+	protected void setCurrency(Currency currency) {
+		if (currency == null){
+            return;
+        }
+
+		this.currency = currency;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	protected void setAmount(BigDecimal amount) throws AccountException {
+		if (amount.compareTo(new BigDecimal(0)) >= 0) {
+            this.amount = amount;
+        }
+	}
+	
+	public void setAccountName(String accountName) {
+        if (accountName != null) {
+            this.accountName = accountName;
+        }
+	}
+	
+	public String getAccountName() {
+		return this.accountName;
+	}
+
+    public abstract void withdrawMoney(BigDecimal money);
+
+    public abstract void insertMoney(BigDecimal money);
+
+    public abstract void generateTask(User user);
+
+}
