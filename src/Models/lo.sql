f42b8e9710af9c7f4ee7b7bc48de51e0c559fcaf
Alexander Zorov <noexile@gmail.com>
2016-02-18 11:04:28 +0200

    the project's db scheme

---
+++ src/Models/lo.sql
@@ -0,0 +1,37 @@
+CREATE SCHEMA `lo` DEFAULT CHARACTER SET utf8; #new schema Life Organizer
+
+CREATE TABLE lo.users (
+user_id int PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(25) UNIQUE NOT NULL,
+pass VARCHAR(25) NOT NULL,
+email VARCHAR(25) UNIQUE NOT NULL
+);
+
+CREATE TABLE lo.payment_events (
+pe_id int PRIMARY KEY AUTO_INCREMENT,
+user_id int NOT NULL,
+pe_name VARCHAR(25) NOT NULL,
+description VARCHAR(255), 
+ammount DOUBLE PRECISION UNSIGNED,
+is_payed BOOLEAN NOT NULL,
+is_overdue BOOLEAN NOT NULL,
+for_date DATE NOT NULL,
+for_time TIME NOT NULL,
+FOREIGN KEY (user_id) REFERENCES lo.users(user_id)
+);
+
+CREATE TABLE lo.todos (
+pe_id int PRIMARY KEY AUTO_INCREMENT,
+user_id int NOT NULL,
+pe_name VARCHAR(25) NOT NULL,
+description VARCHAR(255),
+FOREIGN KEY (user_id) REFERENCES lo.users(user_id)
+);
+
+CREATE TABLE lo.shopping_lists (
+pe_id int PRIMARY KEY AUTO_INCREMENT,
+user_id int NOT NULL,
+item_name VARCHAR(100) NOT NULL,
+item_value DOUBLE PRECISION,
+FOREIGN KEY (user_id) REFERENCES lo.users(user_id)
+);
