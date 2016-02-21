9e8ac2ac6365c39b588e3cfc4a159bcc5c39f7da
Alexander Zorov <noexile@gmail.com>
2016-02-21 23:33:41 +0200

    Created DBManager witch creates schema and adds tables to it.

--- src/Models/dataBase/DBManager.java
+++ src/Models/dataBase/DBManager.java
@@ -5,114 +5,111 @@ import models.interfaces.IDBManager;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
 
 public class DBManager implements IDBManager {
 
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost:3306/?useSSL=false";
+	static final String TABLE_URL = "jdbc:mysql://localhost:3306/lo?useSSL=false";
+	
+	static final String USER = "orgos";
+	static final String PASS = "admin";
+	
+
+	static Connection conn = null;
+	static Statement stmt = null;
+	   
 	public static void main(String[] args) {
 		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(JDBC_DRIVER);
 			System.out.println("Driver loaded successfully");
 		} catch (ClassNotFoundException e) {
 			System.out.println("No such driver imported");
 		}
 		
-		//url = jdbc:mysql://<ip>:<port>/<dbName>;
-		final String url = "jdbc:mysql://localhost:3306/hr?autoReconnect=true&useSSL=false";
-		final String user = "orgos";
-		final String pass = "admin";
+		createSchema();
+		addTablesToSchema();
 		
-		Connection con = null;
+	}
+		
+	private static void addTablesToSchema() {
+		try {
+			conn = DriverManager.getConnection(TABLE_URL, USER, PASS);
+			System.out.println("connection to lo schema successfully");
+			
+		} catch (SQLException e) {
+			System.out.println("Something went wrong with the connection to lo: "  + e.getMessage());
+		}
+		
+		try {	
+			PreparedStatement st;
+			String users = "CREATE TABLE lo.users (user_id int PRIMARY KEY AUTO_INCREMENT, username VARCHAR(25) UNIQUE NOT NULL, pass VARCHAR(25) NOT NULL, email VARCHAR(25) UNIQUE NOT NULL)";
+			st = conn.prepareStatement(users);
+			st.executeUpdate(users);
+			
+			String payment_events = "CREATE TABLE lo.payment_events (pe_id int PRIMARY KEY AUTO_INCREMENT, user_id int NOT NULL, pe_name VARCHAR(25) NOT NULL, description VARCHAR(255), ammount DOUBLE PRECISION UNSIGNED, is_payed BOOLEAN NOT NULL, for_date DATE NOT NULL, for_time TIME NOT NULL, FOREIGN KEY (user_id) REFERENCES lo.users(user_id))";
+			st = conn.prepareStatement(payment_events);
+			st.executeUpdate(payment_events);
+			
+			String todos = "CREATE TABLE lo.todos (pe_id int PRIMARY KEY AUTO_INCREMENT, user_id int NOT NULL, pe_name VARCHAR(25) NOT NULL, description VARCHAR(255), FOREIGN KEY (user_id) REFERENCES lo.users(user_id))";
+			st = conn.prepareStatement(todos);
+			st.executeUpdate(todos);
+			
+			String notification_events = "CREATE TABLE lo.notification_events (ne_id int PRIMARY KEY AUTO_INCREMENT, user_id int NOT NULL, ne_name VARCHAR(25) NOT NULL, description VARCHAR(255), for_date DATE NOT NULL, FOREIGN KEY (user_id) REFERENCES lo.users(user_id))";
+			st = conn.prepareStatement(notification_events);
+			st.executeUpdate(notification_events);
+			
+			String shopping_lists = "CREATE TABLE lo.shopping_lists (pe_id int PRIMARY KEY AUTO_INCREMENT, list_id int NOT NULL, list_name VARCHAR(25) NOT NULL, item_name VARCHAR(100) NOT NULL, item_value DOUBLE PRECISION, user_id int NOT NULL, FOREIGN KEY (user_id) REFERENCES lo.users(user_id))";
+			st = conn.prepareStatement(shopping_lists);
+			st.executeUpdate(shopping_lists);
+			
+			System.out.println("Tables created succesfully");
+		} catch (SQLException e) {
+			System.out.println("Creation of tables failed!");
+			e.printStackTrace();
+		}
 		
 		try {
-			con  = DriverManager.getConnection(url, user, pass);
+			if(conn != null) {
+				conn.close();
+				System.out.println("connection closed");
+			}
+		} catch(SQLException se){
+			se.printStackTrace();
+		}
+	}
+
+	private static void createSchema() {
+		
+		try {
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
 			System.out.println("connection created successfully");
 			
 		} catch (SQLException e) {
 			System.out.println("Something went wrong with the connection : "  + e.getMessage());
 		}
 		
-		try{
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("SELECT department_name FROM hr.departments;");
-			System.out.println("query successfull");
+		try {
+			String createSchema = "CREATE SCHEMA `lo` DEFAULT CHARACTER SET utf8";
+			PreparedStatement st = conn.prepareStatement(createSchema);
+			st.executeUpdate(createSchema);
 			
-			boolean firstEntry = true;
-			int counter = 1;
-			while(rs.next()) {
-				if (firstEntry) {
-					System.out.println("Departments: " );
-					firstEntry = false;
-				}
-				System.out.print(counter++ + ". ");
-				System.out.print(rs.getString("department_name"));
-				System.out.println();
-			}
-			
-//			System.out.println("Updating!");
-//			String name = "Pesho";
-//			String family = "Peshev";
-//			int age = new Random().nextInt(20) + 25;
-//			int teacher_id = 4;
-//			
-//			String query = "INSERT INTO STUDENTS (name, family, age, teacher_id) VALUES ( ?, ?, ?, ?)";
-//			Set<Product> products = new HashSet();
-//			
-//			for(Product s : products) {
-//				PreparedStatement pst = con.prepareStatement(query);
-//				pst.setString(1, s.getName());
-//				pst.setString(2, family);
-//				pst.setInt(3, age);
-//				pst.setInt(4, teacher_id);
-//				pst.executeUpdate();
-//			}
-			
-//			daiPari(con, "Garo");
-			
+			System.out.println("Schema created successfully.");
 		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("Something went wrong with a statemtnt : "  + e.getMessage());
+			System.out.println("Creation of schema failed!");
 		}
 		
 		try {
-			con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			if(conn != null) {
+				conn.close();
+				System.out.println("connection closed");
+			}
+		} catch(SQLException se){
+			se.printStackTrace();
 		}
-		
-	}
 	
-	private static void daiPari(Connection con, String from) throws SQLException {
-		
-		con.setAutoCommit(false);
-		try{
-			PreparedStatement st = con.prepareStatement("UPDATE STUDENTS SET balance = balance - 1000 WHERE name = ?");
-			st.setString(1, from);
-			st.executeQuery();
-			
-			st = con.prepareStatement("UPDATE STUDENTS SET balance = balance + 1000 WHERE name = ?");
-			st.setString(1, "Krasi");
-			st.executeQuery();
-			
-			con.commit();
-		}
-		catch(SQLException e) {
-			con.rollback();
-			throw e;
-		}
-		finally {
-			con.setAutoCommit(true);
-		}
-		
-		//update garo`s money to balance - 1000
-		//updates my money to balance + 1000
-		
 	}
 }
