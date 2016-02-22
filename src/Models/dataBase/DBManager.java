aa9c5d11238b7968a70b1d60eeeaae8427cbf00b
Alexander Zorov <noexile@gmail.com>
2016-02-22 07:22:14 +0200

    removed double-used code and fixed the file to contain only 1 connection to the db

--- src/Models/dataBase/DBManager.java
+++ src/Models/dataBase/DBManager.java
@@ -12,7 +12,6 @@ public class DBManager implements IDBManager {
 
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
 	static final String DB_URL = "jdbc:mysql://localhost:3306/?useSSL=false";
-	static final String TABLE_URL = "jdbc:mysql://localhost:3306/lo?useSSL=false";
 	
 	static final String USER = "orgos";
 	static final String PASS = "admin";
@@ -31,8 +30,25 @@ public class DBManager implements IDBManager {
 			System.out.println("No such driver imported");
 		}
 		
-		createSchema();
-		addTablesToSchema();
+		try {
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			System.out.println("Connection to database was successfully");
+			
+		} catch (SQLException e) {
+			System.out.println("Something went wrong with the connection to the database: "  + e.getMessage());
+		}
+		
+		createSchema(conn);
+		addTablesToSchema(conn);
+		
+		try {
+			if(conn != null) {
+				conn.close();
+				System.out.println("connection closed");
+			}
+		} catch(SQLException se){
+			se.printStackTrace();
+		}
 	}
 	
 	public static DBManager getInstance() {
@@ -42,81 +58,51 @@ public class DBManager implements IDBManager {
 	      return instance;
 	}
 		
-	private static void addTablesToSchema() {
-		try {
-			conn = DriverManager.getConnection(TABLE_URL, USER, PASS);
-			System.out.println("connection to lo schema successfully");
-			
-		} catch (SQLException e) {
-			System.out.println("Something went wrong with the connection to lo: "  + e.getMessage());
-		}
+	private static void addTablesToSchema(Connection conn) {
 		
 		try {	
-			PreparedStatement st;
+			PreparedStatement statement;
 			String users = "CREATE TABLE lo.users (user_id int PRIMARY KEY AUTO_INCREMENT, username VARCHAR(25) UNIQUE NOT NULL, pass VARCHAR(25) NOT NULL, email VARCHAR(25) UNIQUE NOT NULL)";
-			st = conn.prepareStatement(users);
-			st.executeUpdate(users);
+			statement = conn.prepareStatement(users);
+			statement.executeUpdate(users);
+			System.out.println("Table created: users");
 			
 			String payment_events = "CREATE TABLE lo.payment_events (pe_id int PRIMARY KEY AUTO_INCREMENT, user_id int NOT NULL, pe_name VARCHAR(25) NOT NULL, description VARCHAR(255), ammount DOUBLE PRECISION UNSIGNED, is_payed BOOLEAN NOT NULL, for_date DATE NOT NULL, for_time TIME NOT NULL, FOREIGN KEY (user_id) REFERENCES lo.users(user_id))";
-			st = conn.prepareStatement(payment_events);
-			st.executeUpdate(payment_events);
+			statement = conn.prepareStatement(payment_events);
+			statement.executeUpdate(payment_events);
+			System.out.println("Table created: payment_events");
 			
 			String todos = "CREATE TABLE lo.todos (pe_id int PRIMARY KEY AUTO_INCREMENT, user_id int NOT NULL, pe_name VARCHAR(25) NOT NULL, description VARCHAR(255), FOREIGN KEY (user_id) REFERENCES lo.users(user_id))";
-			st = conn.prepareStatement(todos);
-			st.executeUpdate(todos);
+			statement = conn.prepareStatement(todos);
+			statement.executeUpdate(todos);
+			System.out.println("Table created: todos");
 			
 			String notification_events = "CREATE TABLE lo.notification_events (ne_id int PRIMARY KEY AUTO_INCREMENT, user_id int NOT NULL, ne_name VARCHAR(25) NOT NULL, description VARCHAR(255), for_date DATE NOT NULL, FOREIGN KEY (user_id) REFERENCES lo.users(user_id))";
-			st = conn.prepareStatement(notification_events);
-			st.executeUpdate(notification_events);
+			statement = conn.prepareStatement(notification_events);
+			statement.executeUpdate(notification_events);
+			System.out.println("Table created: notification_events");
 			
 			String shopping_lists = "CREATE TABLE lo.shopping_lists (pe_id int PRIMARY KEY AUTO_INCREMENT, list_id int NOT NULL, list_name VARCHAR(25) NOT NULL, item_name VARCHAR(100) NOT NULL, item_value DOUBLE PRECISION, user_id int NOT NULL, FOREIGN KEY (user_id) REFERENCES lo.users(user_id))";
-			st = conn.prepareStatement(shopping_lists);
-			st.executeUpdate(shopping_lists);
+			statement = conn.prepareStatement(shopping_lists);
+			statement.executeUpdate(shopping_lists);
+			System.out.println("Table created: shopping_lists");
 			
-			System.out.println("Tables created succesfully");
+			System.out.println("All tables created succesfully");
 		} catch (SQLException e) {
 			System.out.println("Creation of tables failed!");
 			e.printStackTrace();
 		}
-		
-		try {
-			if(conn != null) {
-				conn.close();
-				System.out.println("connection closed");
-			}
-		} catch(SQLException se){
-			se.printStackTrace();
-		}
 	}
 
-	private static void createSchema() {
-		
-		try {
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
-			System.out.println("connection created successfully");
-			
-		} catch (SQLException e) {
-			System.out.println("Something went wrong with the connection : "  + e.getMessage());
-		}
-		
+	private static void createSchema(Connection conn) {
 		try {
 			String createSchema = "CREATE SCHEMA `lo` DEFAULT CHARACTER SET utf8";
-			PreparedStatement st = conn.prepareStatement(createSchema);
-			st.executeUpdate(createSchema);
-			
+			PreparedStatement statement = conn.prepareStatement(createSchema);
+			statement.executeUpdate(createSchema);
 			System.out.println("Schema created successfully.");
+			
 		} catch (SQLException e) {
 			System.out.println("Creation of schema failed!");
-		}
-		
-		try {
-			if(conn != null) {
-				conn.close();
-				System.out.println("connection closed");
-			}
-		} catch(SQLException se){
-			se.printStackTrace();
-		}
-	
+		}	
 	}
 }
